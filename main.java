68cfd7159942ff8474f3b99bd23005c32bf2c945
ajmeerakalyan <ajmeerakalyan733@gmail.com>
2024-11-12 10:26:59 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Test
+  {
+    public static void main(String[] args)
+    {
+      Scanner sc = new Scanner(System.in);
+      int X = sc.nextInt();
+      int Y = sc.nextInt();
+      int Sum = X+Y;
+      System.out.println(Sum);
+    }
+  }
